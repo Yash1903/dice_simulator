2d77be11ff86a3969264f8d09ba716513782d53f
Yash Prajapati <71633354+Yash1903@users.noreply.github.com>
2020-11-23 11:38:01 -0500

    Add files via upload

---
+++ dice_simulator.py
@@ -0,0 +1,78 @@
+import random
+
+
+
+x = "y"
+
+
+while x == "y":
+
+    number = random.randint(1,8)
+
+    if number == 1:
+            print("**********")
+            print("*        *")
+            print("*        *")
+            print("*   0    *")
+            print("*        *")
+            print("*        *")
+            print("**********")
+    if number == 2:
+            print("**********")
+            print("*        *")
+            print("*        *")
+            print("*0      0*")
+            print("*        *")
+            print("*        *")
+            print("**********")
+    if number == 3:
+            print("**********")
+            print("*   0    *")
+            print("*        *")
+            print("*   0    *")
+            print("*        *")
+            print("*   0    *")
+            print("**********")
+    if number == 4:
+            print("**********")
+            print("*0      0*")
+            print("*        *")
+            print("*        *")
+            print("*        *")
+            print("*0      0*")
+            print("**********")
+    if number == 5:
+            print("**********")
+            print("*0      0*")
+            print("*        *")
+            print("*   0    *")
+            print("*        *")
+            print("*0      0*")
+            print("**********")
+    if number == 6:
+            print("**********")
+            print("*0      0*")
+            print("*        *")
+            print("*  0 0   *")
+            print("*        *")
+            print("*0      0*")
+            print("**********")
+    if number == 7:
+            print("**********")
+            print("*0      0*")
+            print("*   0    *")
+            print("*  0 0   *")
+            print("*        *")
+            print("*0      0*")
+            print("**********")
+    if number == 8:
+            print("**********")
+            print("*0      0*")
+            print("*   0    *")
+            print("*  0 0   *")
+            print("*   0    *")
+            print("*0      0*")
+            print("**********")
+
+    x = input ("run again for press y")
+
